cb4e7eeb977848194e0af2a67cb4289cc42c8366
abtheo <c.theo.kent@gmail.com>
2019-11-28 15:48:46 +0100

    Updated Env to correspond with new code files

--- Main.py
+++ Main.py
@@ -23,7 +23,7 @@ from Agents import NeurosmashAgent
 
 environment = NeurosmashEnvironment()
 agent = NeurosmashAgent()
-agent.load_state_dict(torch.load("D:/AI/Neurosmash/Brains/model.pt", map_location=torch.device('cpu')))
+agent.load_state_dict(torch.load("D:/AI/Neurosmash/Brains/model_3.pt", map_location=torch.device('cpu')))
 
 
 NUM_STEPS = 1000
@@ -34,14 +34,16 @@ experience_replay = []
 
 info, reward, state = environment.init_env()
 for _ in range(NUM_STEPS):
-    action = agent.step(state)
+    state_norm = [s/255 for s in state]
+    action = agent.step(state_norm)
     #action = random.randint(0,2)
     values, indices = torch.max(action, 1)
     print(action)
     print("Values", values, "Indices", indices)
     
     game_over, reward, state = environment.step_safe(indices)
-    
+    if reward == 0:
+        game_over = True
     rewards.append(reward)
     
     game_memory.append([state, action])
@@ -49,15 +51,11 @@ for _ in range(NUM_STEPS):
     if game_over:
         print(sum(rewards))
         if 100 in rewards:
-            experience_replay.append(game_memory)
-
-        
+            experience_replay.append(game_memory)   
         
         game_memory = []
         rewards = []
         info, reward, state = environment.init_env()
-
-        
         
 plt.plot(rewards)
 plt.show()
